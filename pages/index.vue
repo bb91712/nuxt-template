bf2c609024b8c9eae1e53edc06d4b681f5341d40
Your Name <bbx91712@gmail.com>
2021-11-06 23:48:17 -0400

    add pic, updated title

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class='mt-20'>
       <Logo style='width:120px;margin:auto' />
       <h1 class='text-center text-3xl mt-10'>
-        NuxtJS KickStart Template
+        BB Test Page Part 1
       </h1>
       <h2 class='text-center'>
         Create simple, beautiful, and elegant website.
